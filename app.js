f34e00f3cc0a2e244d88f0f633f174c4c940e891
muhtarudinsiregar <6048482+muhtarudinsiregar@users.noreply.github.com>
2018-11-15 15:40:44 +0700

    Change port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const place = require('./routes/place.route');
 const app = express();
 app.use('/place', place);
 
-let port = 1234;
+let port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log('Server is up and running on port numner ' + port);
 });
