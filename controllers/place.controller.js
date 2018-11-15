3b9087e22a5dc9fb646933e70c97b2968eed3476
muhtarudinsiregar <6048482+muhtarudinsiregar@users.noreply.github.com>
2018-11-15 18:53:15 +0700

    Feat: update, delete, show, get all places

--- controllers/place.controller.js
+++ controllers/place.controller.js
@@ -1,5 +1,21 @@
 const Place = require('../models/place.model');
 
+exports.all = function (req, res) {
+    Place.find({}, function (err, places) {
+        var placeMapping = {};
+
+        places.forEach(function (place) {
+            placeMapping[place._id] = place;
+        });
+
+        res.json({
+            status: 'success',
+            message: 'Success get all places',
+            data: placeMapping
+        });
+    });
+};
+
 exports.store = function (req, res) {
     let place = new Place(
         {
@@ -13,6 +29,29 @@ exports.store = function (req, res) {
         if (err) return console.error(err);
         res.send('Place Created successfully')
     })
+};
+
+exports.update = function (req, res) {
+    Place.findByIdAndUpdate(req.params.id, { $set: req.body }, function (err, place) {
+        if (err) return next(err);
+
+        res.json({
+            status: 'success',
+            message: 'Update successfully!'
+        });
+    });
+};
+
+exports.show = function (req, res) {
+    Place.findById(req.params.id, function (err, place) {
+        if (err) return next(err);
+
+        res.json({
+            status: 'success',
+            message: 'Update successfully!',
+            data: place
+        });
+    });
 };
 
 exports.destroy = function (req, res) {
--- routes/place.route.js
+++ routes/place.route.js
@@ -4,7 +4,11 @@ const router = express.Router();
 // Require the controllers WHICH WE DID NOT CREATE YET!!
 const place_controller = require('../controllers/place.controller');
 
+router.get('/all', place_controller.all);
+
 router.post('/store', place_controller.store);
-// router.get('/:id/update', place_controller.update);
+router.put('/:id/update', place_controller.update);
+
+router.get('/:id', place_controller.show);
 router.delete('/:id/destroy', place_controller.destroy);
 module.exports = router;
