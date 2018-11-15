29a33e964de22583a9549a9cf74c3e1d28913e0b
muhtarudinsiregar <6048482+muhtarudinsiregar@users.noreply.github.com>
2018-11-15 17:47:30 +0700

    Setup dotenv for mongodb

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 //app.js
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const place = require('./routes/place.route');
 const app = express();
 
-// setup mongoose
-let db_url = 'mongodb://<dbuser>:<dbpassword>@ds163176.mlab.com:63176/travelr';
-const mongoDB = process.env.MONGODB_URI || db_url;
+// Set up mongoose connection
+const mongoose = require('mongoose');
+const DB_USERNAME = process.env.DB_USERNAME
+const DB_PASSWORD = process.env.DB_PASSWORD
+
+let dev_db_url = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@ds163176.mlab.com:63176/travelr`;
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
@@ -20,9 +26,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/place', place);
-
+app.get('/', (req, res) => {
+    res.send(process.env.DB_USERNAME);
+})
 let port = process.env.PORT || 3000;
 
 app.listen(port, () => {
+    console.log(process.env.DB_USERNAME);
+
     console.log('Server is up and running on port numner ' + port);
 });
